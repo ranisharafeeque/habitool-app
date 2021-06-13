6df5d31a92c28c90946c169eceabee26b860b81f
ranisharafeeque <ranisharafeeque@gmail.com>
2021-06-13 19:04:47 -0400

    finished mongo

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log("hello")
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * start server
+ */
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}...`);
+  });
+  
+  module.exports = app;
